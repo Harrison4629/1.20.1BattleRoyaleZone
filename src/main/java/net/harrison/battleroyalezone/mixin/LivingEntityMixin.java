862395062c2edb66f2572abc58f852e8fa01ed89
Harrison <gamerulekeep1234@gmail.com>
2025-07-07 09:25:52 +0800

    毒圈伤害计算方式修改为每秒固定值

--- src/main/java/net/harrison/battleroyalezone/mixin/LivingEntityMixin.java
+++ src/main/java/net/harrison/battleroyalezone/mixin/LivingEntityMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.border.WorldBorder;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
@@ -20,5 +21,22 @@ public class LivingEntityMixin {
         return bb.minX > wb.getMinX() && bb.maxX < wb.getMaxX()
                 && bb.minZ > wb.getMinZ() && bb.maxZ < wb.getMaxZ();
     }
+
+    @ModifyArg(
+            method = {"baseTick"},
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z",
+                    ordinal = 1
+            ),
+            index = 1
+    )
+    private float modifyBaseDamage(float originalDamage) {
+
+       LivingEntity livingEntity = ((LivingEntity)(Object)this);
+
+       return (float) livingEntity.level().getWorldBorder().getDamagePerBlock();
+    }
+
 
 }
